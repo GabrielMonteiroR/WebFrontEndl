899cc7217ed79eb2f120477cdf57f31509f17172
Gabriel <gmonteiro810@gmail.com>
2024-05-29 20:59:20 -0300

    TDE03

---
+++ Aula02/Aula/aula-tads/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return <div>
+    <h1 className="title">
+      Primeiro project em ReactJS
+    </h1>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVEm_bkYzX5z8t6my1NFmS5yTPK-HgI8P7fQ&s" alt="logo do santos"/>
+  </div>
+}
+
+
+export default App;
---
+++ Aula03/TDE/tde-03/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  return <div>
+    <div className="headerDiv">
+      <div className="HeaderTexts">
+        <img className="Logo" src="https://api.logo.com/api/v2/images?logo=logo_7c6344a9-5343-439f-9a9c-6f269c005037&u=1717021078488&format=svg&margins=166&width=1000&height=750&fit=contain" />
+        <h1>A great Cake</h1>
+        <h2>Changes Everthing</h2>
+      </div>
+      <div className="divCake">
+        <img src="https://th.bing.com/th/id/OIG4.3rXWy4tNMoCZDqs68TId?w=1024&h=1024&rs=1&pid=ImgDetMain" />
+      </div>
+    </div>
+    <div className="middle">
+      <h1>Get started for as little as</h1>
+      <h2>$7.99 PER SERVING</h2>
+      <p>With high-quality ingredients packed into every box, these delicious recipes deliver on value. If you do the math, making these recipes is cheaper with us than doing it on your own.</p>
+    </div>
+    <h3>VIEW CAKES</h3>Y
+
+
+  </div>
+}
+
+
+export default App;
